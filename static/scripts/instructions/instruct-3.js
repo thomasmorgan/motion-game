a9afeec511db94e60df07c341a8b693161ff95c4
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-12-31 11:58:44 -0700

    instructions 3 part 1

--- static/scripts/instructions/instruct-3.js
+++ static/scripts/instructions/instruct-3.js
@@ -7,28 +7,10 @@ $(document).ready(function() {
         allow_exit();
         go_to_page("instructions/instruct-2");
     });
-    reqwest({
-        url: "/experiment/trials_per_round",
-        method: 'get',
-        type: 'json',
-        success: function (resp) {
-            trials_per_round = resp.trials_per_round;
-            $(".trials").html(trials_per_round);
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    });
-    reqwest({
-        url: "/experiment/rounds",
-        method: 'get',
-        type: 'json',
-        success: function (resp) {
-            rounds = resp.rounds;
-            $(".rounds").html(rounds);
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    });
+});
+
+$(function () {
+    var paper = new Raphael($(".canvas-div").get(0), 400, 400);
+    var rect = paper.rect(0, 0, 400, 400);
+    rect.attr("fill", "#eee");
 });
