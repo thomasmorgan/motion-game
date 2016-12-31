cf6c7e0243f638a0616caee401bb1f067a528529
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-12-31 11:41:33 -0700

    instructions 1 and 2

--- static/scripts/instructions/instruct-2.js
+++ static/scripts/instructions/instruct-2.js
@@ -8,3 +8,43 @@ $(document).ready(function() {
         go_to_page("instructions/instruct-1");
     });
 });
+
+$(function () {
+
+    var paper = new Raphael($(".canvas-div").get(0), 400, 400);
+    var rect = paper.rect(0, 0, 400, 400);
+    rect.attr("fill", "#eee");
+
+    x_offset = $(".canvas-div").position().left;
+    y_offset = $(".canvas-div").position().top;
+
+    ready = true;
+
+    $(".canvas-div").click(function(click_location) {
+        if (ready === true) {
+            ready = false;
+            var circle = paper.circle(click_location.pageX - x_offset, click_location.pageY - y_offset, 10);
+            circle.attr('fill', '#000');
+
+            $(".canvas-div").mousemove(function( event ) {
+
+                x_cor = event.pageX - x_offset;
+                y_cor = event.pageY - y_offset;
+                circle.attr({
+                    cx: x_cor,
+                    cy: y_cor
+                });
+            });
+
+            setTimeout(
+                function() {
+                    $(".canvas-div").mousemove(function( event ) { });
+                    circle.remove();
+                    ready = true;
+                },
+                5000
+            );
+        }
+
+    });
+});
