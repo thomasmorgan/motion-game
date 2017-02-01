5f560483f3143412b7b2b8f6a0c018a4af7585f6
thomasmorgan <thomas.j.h.morgan@gmail.com>
2017-02-01 09:38:20 -0700

    fix bonus calculation

--- experiment.py
+++ experiment.py
@@ -112,7 +112,7 @@ class MotionGame(Experiment):
         """Calculate the bonus payment for participants."""
         nets = [n.id for n in Network.query.filter_by(role="experiment").all()]
         total_points = sum([max(n.points - 20, 0) for n in participant.nodes() if n.network_id in nets])
-        return min(round(float(total_points)/(30*len(nets)), 2), 1.00)*config.max_bonus
+        return round(min(float(total_points)/(30*len(nets)), 1.00)*config.max_bonus, 2)
 
     def attention_check(self, participant):
         nets = [n.id for n in Network.query.filter_by(role="catch").all()]
