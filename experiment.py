552de7318e00ff7cd16722de745f614b569a16a0
thomasmorgan <thomas.j.h.morgan@gmail.com>
2017-03-02 17:08:58 -0700

    refactor hausdorff

--- experiment.py
+++ experiment.py
@@ -164,36 +164,10 @@ def get_hausdorff():
     true_ys = contents["true_ys"]
     true_ts = contents["true_ts"]
 
-    # calculate the location of the dot at evenly spaced intervals
-    ts_2 = range(0, 5000, config.get("hausdorff_interval"))
-    xs_2 = []
-    ys_2 = []
-    true_xs_2 = []
-    true_ys_2 = []
+    import hausdorff
 
-    for t in ts_2:
-        index = len(ts) - 1 - [time <= t for time in ts][::-1].index(True)
-        xs_2.append(xs[index])
-        ys_2.append(ys[index])
-
-        index = len(true_ts) - 1 - [time <= t for time in true_ts][::-1].index(True)
-        true_xs_2.append(true_xs[index])
-        true_ys_2.append(true_ys[index])
-
-    # calculate the velocity of the dot at these intervals
-    ts_3 = ts_2[1:]
-    xs_3 = [xs_2[i+1] - xs_2[i] for i in range(len(xs_2)-1)]
-    ys_3 = [ys_2[i+1] - ys_2[i] for i in range(len(ys_2)-1)]
-    true_xs_3 = [true_xs_2[i+1] - true_xs_2[i] for i in range(len(true_xs_2)-1)]
-    true_ys_3 = [true_ys_2[i+1] - true_ys_2[i] for i in range(len(true_ys_2)-1)]
-
-    # calculate the hausdorff distance
-    hausdorff = 0
-    for x, y, t in zip(xs_3, ys_3, ts_3):
-        closest = int(round(min([pow(pow(x-tx, 2) + pow(y-ty, 2) + pow((t-tt)/config.get("ms_per_px"), 2), 0.5) for tx, ty, tt in zip(true_xs_3, true_ys_3, ts_3)])))
-        if closest > hausdorff:
-            hausdorff = closest
+    hd = hausdorff.hausdorff(xs, ys, ts, true_xs, true_ys, true_ts)
 
     data = {"status": "success",
-            "hausdorff": hausdorff}
+            "hausdorff": hd}
     return Response(json.dumps(data), status=200, mimetype='application/json')
--- models.py
+++ models.py
@@ -192,39 +192,12 @@ class MotionAgent(Agent):
         true_ys = dat["true_ys"]
         true_ts = dat["true_ts"]
 
-        # calculate the location of the dot at evenly spaced intervals
-        ts_2 = range(0, 5000, config.get("hausdorff_interval"))
-        xs_2 = []
-        ys_2 = []
-        true_xs_2 = []
-        true_ys_2 = []
-
-        for t in ts_2:
-            index = len(ts) - 1 - [time <= t for time in ts][::-1].index(True)
-            xs_2.append(xs[index])
-            ys_2.append(ys[index])
-
-            index = len(true_ts) - 1 - [time <= t for time in true_ts][::-1].index(True)
-            true_xs_2.append(true_xs[index])
-            true_ys_2.append(true_ys[index])
-
-        # calculate the velocity of the dot at these intervals
-        ts_3 = ts_2[1:]
-        xs_3 = [xs_2[i+1] - xs_2[i] for i in range(len(xs_2)-1)]
-        ys_3 = [ys_2[i+1] - ys_2[i] for i in range(len(ys_2)-1)]
-        true_xs_3 = [true_xs_2[i+1] - true_xs_2[i] for i in range(len(true_xs_2)-1)]
-        true_ys_3 = [true_ys_2[i+1] - true_ys_2[i] for i in range(len(true_ys_2)-1)]
-
-        # calculate the hausdorff distance
-        hausdorff = 0
-        for x, y, t in zip(xs_3, ys_3, ts_3):
-            closest = int(round(min([pow(pow(x-tx, 2) + pow(y-ty, 2) + pow((t-tt)/config.get("ms_per_px"), 2), 0.5) for tx, ty, tt in zip(true_xs_3, true_ys_3, ts_3)])))
-            if closest > hausdorff:
-                hausdorff = closest
+        import hausdorff
+        hd = hausdorff.hausdorff(xs, ys, ts, true_xs, true_ys, true_ts)
 
         # save the results
-        self.error = hausdorff
-        self.points = int(round(max(0, 100-round(hausdorff/4))))
+        self.error = hd
+        self.points = int(round(max(0, 100-round(hd/4))))
         social = int(self.infos(type=SocialGene)[0].contents)
         self.fitness = pow(max(self.points - social*config.get("social_cost"), 0), 2)
 
